a8689781b3c8463e338f96151059ca505ab6d1ec
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-07-05 11:31:03 +0530

    created_models

--- employees/models.py
+++ employees/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeForm(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class EmployeeField(models.Model):
+    FIELD_TYPES = [
+        ('text', 'Text'),
+        ('number', 'Number'),
+        ('date', 'Date'),
+        ('password', 'Password'),
+    ]
+
+    form = models.ForeignKey(EmployeeForm, related_name='fields', on_delete=models.CASCADE)
+    label = models.CharField(max_length=100)
+    field_type = models.CharField(max_length=50, choices=FIELD_TYPES)
+    order = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.label} ({self.field_type})"
+
+class Employee(models.Model):
+    form = models.ForeignKey(EmployeeForm, on_delete=models.SET_NULL, null=True)
+    data = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Employee Entry {self.id}"
